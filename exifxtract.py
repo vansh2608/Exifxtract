cbc1ac37d02b4da44d0a3de0b7f4bf41f7db8811
vansh2608 <96186498+vansh2608@users.noreply.github.com>
2022-01-21 20:32:45 +0530

    Update exifxtract.py

--- exifxtract.py
+++ exifxtract.py
@@ -10,7 +10,7 @@ def banner():
 
 banner()
 
-path_type = input("Image URL(Enter 1) or Image Path(Enter 2)\n>")
+path_type = input("Image URL(Enter 1) | Image Path(Enter 2) | Exif Remover(3)\n>")
 
 if path_type == "1":
   url = input("Enter Image URL\n>")
@@ -66,6 +66,25 @@ elif path_type == "2":
         print(f"\033[0;31m{tagname:25}: {value}")
   except:
     print("Enter Valid Path")
+  
+elif path_type == "3":
+  try:
+    image_path = input("Enter Image Name\n>")
+    print("\n")
+    img = Image.open(image_path)
+          # We get the exif data from the which we'll overwrite
+    img_data = list(img.getdata())
+          # We create a new Image object. We initialise it with the same mode and size as the original image.
+    img_no_exif = Image.new(img.mode, img.size)
+          # We copy the pixel data from img_data. 
+    img_no_exif.putdata(img_data)
+          # We save the new image without exif data. The keyword argument exif would've been used with the exif data if you wanted to save any. We overwrite the original image.
+    img_no_exif.save(image_path)
+    print("Exif Data Removed")
+
+  except:
+    print("An Error Occured")
 
 else:
   print("Enter Valid Input")
+
